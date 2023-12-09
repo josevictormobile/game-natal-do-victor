473955c97dff9b7480540236261c729cd7761a82
unknown <josevictormobile@gmail.com>
2023-12-09 01:24:40 -0300

    feat: inserindo menu simples

--- natal_game.lua
+++ natal_game.lua
@@ -1,38 +1,95 @@
--- definicao dos sprites
-spr_santa = 19
-spr_house = 2
-spr_renas = 20
-spr_gift = 3
-spr_bird = 4
+function start_values()
+    -- variaveis de status
+    game_over = false
+    gift_fall = false
+    santa_jump = false
+    jump_height = 95
+    santa_speed = 2
+    score = 0
+    hp = 3
+    menu_fase = true
+    menu_items = {"Jogar", "Reiniciar"}
+    selected_item = 1
 
--- hitboxes
-renas_p = {x = 68, y = 110, w = 8, h = 8}
-gift_p = {x = -8, y = -8}
-santa_p = {x = 60, y = 110, w = 8, h = 8}
-houses = {
-    { x = 120, y = 110, w = 8, h = 8 },
-    { x = 180, y = 110, w = 8, h = 8  },
-    { x = 240, y = 110, w = 8, h = 8  },
-    { x = 160, y = 110, w = 8, h = 8  }
-}
-birds = {
-    {x = 120, y = 50, w = 8, h = 8 },
-    {x = 180, y = 90, w = 8, h = 8 }
-}
+    -- definicao dos sprites
+    spr_santa = 19
+    spr_house = 2
+    spr_renas = 20
+    spr_gift = 3
+    spr_bird = 4
 
--- variaveis de status
-game_over = false
-gift_fall = false
-santa_jump = false
-jump_height = 95
-santa_speed = 2
-score = 0
-hp = 3
+    -- hitboxes
+    renas_p = {x = 68, y = 110, w = 8, h = 8}
+    gift_p = {x = -8, y = -8}
+    santa_p = {x = 60, y = 110, w = 8, h = 8}
+    houses = {
+        { x = 120, y = 110, w = 8, h = 8 },
+        { x = 180, y = 110, w = 8, h = 8  },
+        { x = 240, y = 110, w = 8, h = 8  },
+        { x = 160, y = 110, w = 8, h = 8  }
+    }
+    birds = {
+        {x = 120, y = 50, w = 8, h = 8 },
+        {x = 180, y = 90, w = 8, h = 8 }
+    }
+end
 
 function _init()
     cls()
-    -- carregar os sprites
+    start_values()
     load_sprites()
+end
+
+function _update()
+	check_collision()
+    check_gift_delivery()
+    update_menu()
+    update_santa()
+    update_houses()
+    update_birds()
+    update_gifts()
+end
+
+function _draw()
+    if menu_fase then
+        cls()
+        print("natal do victor", 30, 10, 7)
+        background_draw()
+        menu()
+    elseif game_over then 
+        cls()
+        print("game over! score: " .. score, 30, 10, 7)
+        menu()
+    else
+        cls() -- limpa a tela
+        background_draw()
+       	load_sprites() -- atualizar os sprites na tela
+        print("score:" .. score .."\nhp:" .. hp, 4, 4, 7) -- exibir a pontuacao na tela
+	end
+end
+
+function background_draw()
+    rectfill(0, 118, 127, 127, 7) -- um retangulo branco para o chao com neve
+    pset(10, 78, 7)
+    pset(21, 12, 7)
+    pset(43, 78, 7)
+    pset(46, 49, 7)
+    pset(94, 18, 7)
+    pset(15, 10, 7)
+    pset(87, 45, 7)
+    pset(89, 90, 7)
+    pset(102, 51, 7)
+    pset(47, 35, 7)
+end
+
+function menu()
+    for i = 1, #menu_items do
+        if i == selected_item then
+            print("-> " .. menu_items[i], 30, 30 + i * 10, 7)
+        else
+            print(menu_items[i], 30, 30 + i * 10, 7)
+        end
+    end
 end
 
 function load_sprites()
@@ -48,13 +105,28 @@ function load_sprites()
     end
 end
 
-function _update()
-	check_collision()
-    check_gift_delivery()
-    update_santa()
-    update_houses()
-    update_birds()
-    update_gifts()
+function update_menu()
+    if menu_fase or game_over then
+        if (btnp(2)) selected_item = selected_item - 1
+        if (btnp(3)) selected_item = selected_item + 1
+        
+        if selected_item < 1 then
+            selected_item = 2
+        elseif selected_item > 2 then
+            selected_item = 1
+        end
+
+        if btn(5) then 
+            if selected_item == 1 then --Jogar
+                start_values()
+                menu_fase = false
+                game_over = false
+            elseif selected_item == 2 then --Reiniciar
+                menu_fase = true
+                game_over = false
+            end 
+        end
+    end
 end
 
 function update_santa()
@@ -124,7 +196,7 @@ function check_collision()
     for i = 1, #houses do
         if check_hitbox_collision(santa_p, houses[i]) then
             hp -= 1
-            if hp == 0 then
+            if hp <= 0 then
                 game_over = true
             end
         end
@@ -132,7 +204,7 @@ function check_collision()
     for i = 1, #birds do
         if check_hitbox_collision(santa_p, birds[i]) then
             hp -= 1
-            if hp == 0 then
+            if hp <= 0 then
                 game_over = true
             end
         end
@@ -146,30 +218,4 @@ function check_gift_delivery()
             score += 1
         end
     end
-end
-
-function background_draw()
-    rectfill(0, 118, 127, 127, 7) -- um retれけngulo azul para o cれたu
-    pset(10, 78, 7)
-    pset(21, 12, 7)
-    pset(43, 78, 7)
-    pset(46, 49, 7)
-    pset(94, 18, 7)
-    pset(15, 10, 7)
-    pset(87, 45, 7)
-    pset(89, 90, 7)
-    pset(102, 51, 7)
-    pset(47, 35, 7)
-end
-
-function _draw()
-    if game_over then 
-        cls()
-        print("game over! score: " .. score, 40, 60, 7)
-    else
-        cls() -- limpa a tela
-        background_draw()
-       	load_sprites() -- atualizar os sprites na tela
-        print("score:" .. score .."\nhp:" .. hp, 4, 4, 7) -- exibir a pontuacao na tela
-	end
 end
